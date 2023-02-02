6224f531778ddc7b4ac9668a565090a1d18a48d7
Taming Wild AI <taming.wild.ai@gmail.com>
2023-02-02 15:00:32 +0000

    Removed requirement for intel-tensorflow---not needed on GPU instance.

--- tests/prerequisites.py
+++ tests/prerequisites.py
@@ -9,11 +9,11 @@ class TestPrerequisites(unittest.TestCase):
             31 * 1024 * 1024 * 1024,
             "Prerequisite test failed due to insufficient physical memory.")
 
-    def test_intel_tensorflow(self):
-        from tensorflow.python.util import _pywrap_util_port
-        self.assertTrue(
-            _pywrap_util_port.IsMklEnabled(),
-            "Prerequisite test failed because Intel TensorFlow not detected.")
+    def test_gpu_presence(self):
+        detected = False
+        for device in tf.config.list_physical_devices():
+            detected = detected or device.device_type == 'GPU'
+        self.assertTrue(detected), "Prerequisite test failed because GPU not detected."
 
 if __name__ == "__main__":
     unittest.main()
